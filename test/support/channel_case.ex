e0aa21e40bafbf4b5fbb1573ad033e62fb7e1524
Kim Lindholm <misc@dynevision.com>
2017-10-13 19:29:23 +0700

    Run "mix credo" and fix style issues

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -25,7 +25,6 @@ defmodule SemanticUiPhoenixSassWebpackWeb.ChannelCase do
     end
   end
 
-
   setup tags do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(SemanticUiPhoenixSassWebpack.Repo)
     unless tags[:async] do
