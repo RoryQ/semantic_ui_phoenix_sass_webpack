77030cbc614db2b1a98abb4515fae9bb9daf3cc8
Kim Lindholm <misc@dynevision.com>
2017-10-13 16:32:16 +0700

    Edit Sobelow config, run "mix sobelow --config" and fix found issues

--- config/prod.exs
+++ config/prod.exs
@@ -40,10 +40,10 @@ config :logger, level: :info
 #
 # We also recommend setting `force_ssl`, ensuring no data is
 # ever sent via http, always redirecting to https:
-#
-#     config :semantic_ui_phoenix_sass_webpack, SemanticUiPhoenixSassWebpackWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
+
+config :semantic_ui_phoenix_sass_webpack, SemanticUiPhoenixSassWebpackWeb.Endpoint,
+  force_ssl: [hsts: true]
+
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
 # ## Using releases
