810b3b86cbc452fc2ecfe1fc4b769cb58c89381a
Kim Lindholm <misc@dynevision.com>
2017-10-14 16:27:00 +0700

    Run "yarn remove react react-dom react-hot-loader" in assets folder and remove React

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,3 @@
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
-
-import React from "react";
-import { render } from "react-dom";
-import Hello from "./components/Hello";
-
-render(<Hello />, document.getElementById("app"));
