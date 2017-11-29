c753875a2d418e515c747037a59f1c3eedbfddfe
Kim Lindholm <misc@dynevision.com>
2017-11-29 17:34:38 +0700

    Render React component and verify with "mix phx.server"

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,9 @@ import "phoenix_html";
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import React from "react";
+import { render } from "react-dom";
+import Hello from "./components/Hello";
+
+render(<Hello />, document.getElementById("app"));
