26d7225b57ccea29da38d195c52a3a6e41b10efa
Kim Lindholm <misc@dynevision.com>
2017-12-02 19:43:41 +0700

    Finish adding Semantic UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,30 @@
 
 import "phoenix_html";
 
+import "semantic-ui-sass/js/api";
+import "semantic-ui-sass/js/colorize";
+import "semantic-ui-sass/js/form";
+import "semantic-ui-sass/js/state";
+import "semantic-ui-sass/js/visibility";
+import "semantic-ui-sass/js/visit";
+import "semantic-ui-sass/js/site";
+import "semantic-ui-sass/js/accordion";
+import "semantic-ui-sass/js/checkbox";
+import "semantic-ui-sass/js/dimmer";
+import "semantic-ui-sass/js/dropdown";
+import "semantic-ui-sass/js/embed";
+import "semantic-ui-sass/js/modal";
+import "semantic-ui-sass/js/nag";
+import "semantic-ui-sass/js/popup";
+import "semantic-ui-sass/js/progress";
+import "semantic-ui-sass/js/rating";
+import "semantic-ui-sass/js/search";
+import "semantic-ui-sass/js/shape";
+import "semantic-ui-sass/js/sidebar";
+import "semantic-ui-sass/js/sticky";
+import "semantic-ui-sass/js/tab";
+import "semantic-ui-sass/js/transition";
+
 // Import local files
 //
 // Local files can be imported directly using relative
